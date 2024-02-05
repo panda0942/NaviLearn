f2a47e02c97c4fd228b3658bbe9860e5a45031ce
Herbert <sekpeyherbert@gmail.com>
2024-02-05 11:14:51 +0000

    Setup app complete

---
+++ navilearn/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Section from './nav';
+
+function App() {
+  return (
+    <div className="pad">
+      <Section />
+    </div>
+  );
+}
+
+export default App;
---
+++ navilearn/src/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const Section = ()=>{
+    return(
+        <div>
+        Hello World   
+        </div>
+    )
+}
+
+export default Section
